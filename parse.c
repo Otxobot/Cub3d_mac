863a2af04e919d5f7a621fbcc859b8be1355e066
Otxobot <otxoboy64@gmail.com>
2023-12-14 13:28:01 +0100

    arreglado archivo si no existe

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 12:43:32 by abasante          #+#    #+#             */
-/*   Updated: 2023/12/14 13:15:05 by abasante         ###   ########.fr       */
+/*   Updated: 2023/12/14 13:25:14 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,3 @@ int parse(char **elements_without_empty_lines, t_main *datos, char *argument)
 		return (printf("Error\nmap parse incorrect"), 1);
 	return (0);
 }
-
