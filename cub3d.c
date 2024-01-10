5831def3719d553045e25878d1234dd34f276eb1
Otxobot <otxoboy64@gmail.com>
2024-01-10 15:06:58 +0100

    no va

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 16:53:50 by abasante          #+#    #+#             */
-/*   Updated: 2024/01/10 12:06:51 by abasante         ###   ########.fr       */
+/*   Updated: 2024/01/10 15:04:27 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,12 @@ int	main(int ac, char **av)
 		if (parse(elements_without_empty_lines, &datos, av[1]))
 			return (1);
 		init_values(&datos);
-		init_mlx(&datos);
+		datos.mlx = mlx_init();
+		datos.addr = mlx_new_window(datos.mlx, SCREENWIDTH, \
+		SCREENHEIGHT, "cub3d");
+		datos.image = mlx_new_image(datos.mlx, SCREENWIDTH, SCREENHEIGHT);
+		datos.addr = mlx_get_data_addr(datos.image, \
+		&datos.bits_per_pixel, &datos.size, &datos.endian);
 		init_textures(&datos);
 		load_screen(&datos);
 		exiting = mlx_hook(datos.window, 2, 1L<<0, key_hook, &datos);
