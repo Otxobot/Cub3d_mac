e16e949855edac0941dffa715b51ec243dc3e81b
Otxobot <otxoboy64@gmail.com>
2024-01-11 16:58:35 +0100

    colores del cielo bien puestas

--- textures/init_textures.c
+++ textures/init_textures.c
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 16:07:18 by abasante          #+#    #+#             */
-/*   Updated: 2024/01/11 16:39:54 by abasante         ###   ########.fr       */
+/*   Updated: 2024/01/11 16:54:08 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ int	check_for_texture(char *texture)
 
 	fd = open(texture, O_RDONLY);
 	if (fd == -1)
-	{
-		return (1);
-	}
+		return (printf("Error\nTextura incorrecta\n"), 1);
 	close(fd);
 	return (0);
 }
